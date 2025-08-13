e79f3ec2e932d9469717110d900d28d470a87d4a
Pranav Unkule <unkulepranav1@gmail.com>
2025-08-13 13:47:01 +1000

    Add logger parameter to Trainer class and implement save_model method

--- train/trainer.py
+++ train/trainer.py
@@ -1,3 +1,4 @@
+from typing import Any
 import torch
 from torch.utils.data import DataLoader
 from torch.nn import Module
@@ -15,6 +16,7 @@ class Trainer:
         accumulation_steps: int,
         save_model_path: str,
         log_freq: int,
+        logger: Any,
         device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu"),
     ):
         self.model = model.to(device)
@@ -27,3 +29,14 @@ class Trainer:
         self.save_model_path = save_model_path
         self.log_freq = log_freq
         self.device = device
+        self.logger = logger
+
+    def save_model(self, path=None):
+        path = path or self.save_model_path
+        try:
+            state_dict = self.model.state_dict()
+            torch.save(state_dict, path)
+            if self.logger.is_main:
+                self.logger.print(f"💾 Model saved at {path}")
+        except Exception as e:
+            self.logger.print(f" Save error: {e}")
